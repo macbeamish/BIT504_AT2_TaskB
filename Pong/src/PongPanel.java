6039a04830e56d3fccd49202f4bb8b57136fe02e
macbeamish <75453707+macbeamish@users.noreply.github.com>
2022-01-05 15:20:07 +1300

    Update PongPanel.java
    Increased Ball Move speed, to 5 - Makes the game feel more responsive and enjoyable
    Increased Points to Win to 11-

--- Pong/src/PongPanel.java
+++ Pong/src/PongPanel.java
@@ -16,8 +16,8 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener
 	private final static Color BACKGROUND_COLOR = Color.BLACK;
 	private final static int TIMER_DELAY = 5;
 
-	private static final int BALL_MOVEMENT_SPEED = 3;
-	private final static int POINTS_TO_WIN = 10;
+	private static final int BALL_MOVEMENT_SPEED = 5;
+	private final static int POINTS_TO_WIN = 11;
 	private final static int PADDLE_SPEED = 3;
 
 	int player1Score = 0, player2Score = 0;
